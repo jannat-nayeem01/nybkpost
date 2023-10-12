ce811a19f702460e489e0a27f2c15572e18e0714
Jannatun Nayeem <jannat.nayeem01@gmail.com>
2023-10-12 19:36:47 +0700

    production.rb changes

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,15 +1,34 @@
-class MainController < ApplicationController
-  def index
-    begin
-    # Generate a divide-by-zero exception
-      result = 10 / 0
-    rescue => e
-      # Log the error
-      LogEntry.create(message: "About to divide by 0. Error: #{e.message}")
+require 'nokogiri'
+require 'open-uri'
 
-      # Handle the exception as needed
-      flash[:error] = "An error occurred: #{e.message}"
-    end
+class MainController < ApplicationController
+
+  def divide_by_zero
+    # Log a message
+    Rails.logger.info("About to divide by 0")
+
+    # Raise the exception
+    result = 5 / 0 # This will raise a ZeroDivisionError
   end
+  def index
+    
+        # Scrape New York Times headlines
+        nytimes_url = 'https://www.nytimes.com'
+        nytimes_doc = Nokogiri::HTML(URI.open(nytimes_url))
+        nytimes_headlines = nytimes_doc.css('a[href*="/world/"]')
+    
+        # Scrape Bangkok Post headlines
+        bangkokpost_url = 'https://www.bangkokpost.com'
+        bangkokpost_doc = Nokogiri::HTML(URI.open(bangkokpost_url))
+        bangkokpost_headlines = bangkokpost_doc.css('a[href*="/thailand/"]')
+    
+        render 'main/index', locals: {
+          nytimes_headlines: nytimes_headlines,
+          bangkokpost_headlines: bangkokpost_headlines
+        }
+  end
+
+  
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   #get 'headlines/fetch_headlines'
-  get 'headlines', to: 'headlines#fetch_headlines'
+  #get 'headlines', to: 'headlines#fetch_headlines'
 
   #get 'main/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+   #root "headlines#fetch_headlines"
    root "main#index"
    #get 'headlines/index'
 
